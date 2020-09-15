4bd9e8ffa08fdd823ebe6d441e22f5ffdaece778
Stanley Young <stanleyisyoung@users.noreply.github.com>
2020-09-15 14:55:09 -0700

    notable health-physician list front end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      doctors: {},
+      selectedDoctor: null,
+    };
+  }
+  componentDidMount = async () => {
+    await fetch('http://localhost:3000/api/v1/physicians')
+      .then(function(response) {
+        return response.json();
+      }).then((data) => {
+        this.setState({
+          doctors: data,
+          selectedDoctor: data[0].id, // setting default doctor to first one
+          appointments: []
+        })
+      });
+    let selected = this.state.selectedDoctor
+    fetch(`http://localhost:3000/api/v1/appointments?id=${selected}`)
+      .then(function(response) {
+        return response.json();
+      }).then((data) => {
+        this.setState({
+          isLoaded: true,
+          appointments: data
+        })
+    });
+  }
+
+  getAppointments = (physician_id) => {
+    fetch(`http://localhost:3000/api/v1/appointments?id=${physician_id}`)
+      .then(function(response) {
+        return response.json();
+      }).then((data) => {
+        this.setState({
+          isLoaded: true,
+          appointments: data,
+          selectedDoctor: physician_id,
+        })
+    });
+  }
+
+  renderDoctorsList = (docs) => {
+    let docList = [];
+    for (const [index, value] of docs.entries()){
+      docList.push(<button onClick={() => this.getAppointments(value.id)} key={index}>{value.name}</button>)
+    }
+    return docList;
+  }
+
+  renderAppointments = (appt) => {
+    let apptList = [];
+    for (const [index, value] of appt.entries()){
+      apptList.push(<li key={index}>{value.id} {value.name} {value.time} {value.kind}</li>)
+    }
+    return apptList;
+  }
+
+  render() {
+    if(!this.state.isLoaded) return (<div>Loading...</div>);
+    console.log(this.state);
+    return (
+      <div className="App">
+        <header className="app-body">
+          {/* todo: break into components */}
+          <div className="doctors column">
+            {this.renderDoctorsList(this.state.doctors)}
+          </div>
+          <div className="appointments column">
+            {this.renderAppointments(this.state.appointments)}
+          </div>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
